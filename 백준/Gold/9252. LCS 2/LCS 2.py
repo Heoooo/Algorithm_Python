6bd7d6a2e7a411d9abca993520950a4cba54ccd8
Heoooo <85151561+Heoooo@users.noreply.github.com>
2025-02-10 15:44:05 +0900

    [Gold IV] Title: LCS 2, Time: 460 ms, Memory: 56604 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,16 +1,30 @@
 import sys
 input = sys.stdin.readline
+
 str1 = input().strip()
 str2 = input().strip()
-DP = [[""]*(len(str1)+1) for _ in range(len(str2)+1)]
-for i in range(len(str2)):
-    for j in range(len(str1)):
-        if str2[i] == str1[j]:
-            DP[i+1][j+1] = DP[i][j]+str2[i]
+N, M = len(str1), len(str2)
+
+DP = [[0] * (N + 1) for _ in range(M + 1)]
+
+for i in range(1, M + 1):
+    for j in range(1, N + 1):
+        if str2[i - 1] == str1[j - 1]:
+            DP[i][j] = DP[i - 1][j - 1] + 1
         else:
-            if len(DP[i][j+1]) > len(DP[i+1][j]):
-                DP[i+1][j+1] = DP[i][j+1]
-            else:
-                DP[i+1][j+1] = DP[i+1][j]
-print(len(DP[-1][-1]))
-print(DP[-1][-1])
+            DP[i][j] = max(DP[i - 1][j], DP[i][j - 1])
+print(DP[M][N])
+
+lcs = []
+i, j = M, N
+while i > 0 and j > 0:
+    if str2[i - 1] == str1[j - 1]:
+        lcs.append(str2[i - 1])
+        i -= 1
+        j -= 1
+    elif DP[i - 1][j] > DP[i][j - 1]:
+        i -= 1
+    else:
+        j -= 1
+
+print("".join(reversed(lcs)))
