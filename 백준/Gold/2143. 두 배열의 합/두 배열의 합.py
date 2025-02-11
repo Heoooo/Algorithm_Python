2f210f7427fd3c00d6a47500e99441b383eacd29
Heoooo <85151561+Heoooo@users.noreply.github.com>
2025-02-11 15:50:08 +0900

    [Gold III] Title: 두 배열의 합, Time: 412 ms, Memory: 74400 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -1,26 +1,30 @@
-from itertools import combinations
 import sys
 input = sys.stdin.readline
+
 T = int(input())
 N = int(input())
 nums1 = list(map(int, input().split()))
 M = int(input())
 nums2 = list(map(int, input().split()))
+
 rs = 0
 num = {}
+
 for i in range(N):
     tmp = 0
     for j in range(i, N):
-        tmp = sum(nums1[i:j+1])
+        tmp += nums1[j]
         if tmp not in num:
             num[tmp] = 1
         else:
             num[tmp] += 1
 
+
 for i in range(M):
     tmp = 0
     for j in range(i, M):
-        tmp = sum(nums2[i:j+1])
+        tmp += nums2[j]
         if T - tmp in num:
-            rs += num[T-tmp]
+            rs += num[T - tmp]
+
 print(rs)
