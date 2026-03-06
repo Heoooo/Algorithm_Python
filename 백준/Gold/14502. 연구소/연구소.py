79644e10c108ede615225fcbfc6069ba8b735e68
허진혁 <85151561+Heoooo@users.noreply.github.com>
2026-03-06 10:53:10 +0900

    [Gold IV] Title: 연구소, Time: 1532 ms, Memory: 36040 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,48 +1,55 @@
 import sys
+from itertools import combinations
 from collections import deque
-import copy
+
 input = sys.stdin.readline
-N, M = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(N)]
-dx = [1,0,-1,0]
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
 dy = [0,1,0,-1]
-cnt = 0
+dx = [1,0,-1,0]
+
+wc = []
+vc = []
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            wc.append((i, j))
+        elif graph[i][j] == 2:
+            vc.append((i, j))
+            
+rs = 0
 
 def bfs():
     que = deque()
-    temp_graph = copy.deepcopy(graph)
-    for i in range(N):
-        for j in range(M):
-            if temp_graph[i][j] == 2:
-                que.append((i,j))
+    tmp_graph = [row[:] for row in graph]
+    for y, x in vc:
+        que.append((y, x))
+
     while que:
-        y,x = que.pop()
+        y, x = que.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
             if 0 <= ny < N and 0 <= nx < M:
-                if temp_graph[ny][nx] == 0:
-                    temp_graph[ny][nx] = 2
-                    que.append((ny,nx))
-
-    global cnt
-    tmp = 0
+                if tmp_graph[ny][nx] == 0:
+                    tmp_graph[ny][nx] = 2
+                    que.append((ny, nx))
+    cnt = 0
     for i in range(N):
         for j in range(M):
-            if temp_graph[i][j] == 0:
-                tmp += 1
-    cnt = max(cnt, tmp)
+            if tmp_graph[i][j] == 0:
+                cnt += 1
+    return cnt
 
-def wall(count):
-    if count == 3:
-        bfs()
-        return
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                wall(count+1)
-                graph[i][j] = 0
 
-wall(0)
-print(cnt)    
+for comb in combinations(wc, 3):
+    for y, x in comb:
+        graph[y][x] = 1
+    rs = max(rs, bfs())
+    for y, x in comb:
+        graph[y][x] = 0
+
+print(rs)
